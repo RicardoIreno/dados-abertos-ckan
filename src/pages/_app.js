e3005c42115d571cdf0569534c8e889def70386e
Ricardo Ireno <ricardoireno@outlook.com>
2022-01-21 14:32:00 -0300

    Add react-query and _App adequacy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,35 @@
 import { ThemeProvider } from 'styled-components'
-import {theme} from '../theme'
 import GlobalStyle from '../theme/global'
+import {theme} from '../theme'
 import '../theme/fonts.css'
+import {Hydrate, QueryClient, QueryClientProvider} from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import {useState} from 'react'
+// import queryClient from '../services/query'
+
 
 function MyApp({ Component, pageProps }) {
+  const [queryClient] = useState(
+    () => 
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 20 * 1000,
+          },
+        },
+      })
+  )
+
   return(
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+    <Hydrate state={pageProps.dehydratedState}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </ThemeProvider>
+    </Hydrate>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
   )
   
 }
