db0bf7322ac5e926f4f2cde6bfced9907d05a090
Ricardo Ireno <ricardoireno@outlook.com>
2022-01-22 14:15:43 -0300

    Trying to use headers in api call

--- src/services/apiCkan.ts
+++ src/services/apiCkan.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
-const ApiCkan = axios.create({  
-  baseURL: process.env.NEXT_PUBLIC_BASEURL,
-  headers: { 'Authorization': 'a8b0ff18-2234-4391-80a4-e61168d4419f' }
-  })
+const ApiCkan = axios.create( {baseURL: process.env.NEXT_PUBLIC_BASEURL} )
+
+// const ApiCkan = axios.create({  
+//   baseURL: process.env.NEXT_PUBLIC_BASEURL,
+//   headers: { 'Authorization': 'a8b0ff18-2234-4391-80a4-e61168d4419f' }
+//   })
 export default ApiCkan
