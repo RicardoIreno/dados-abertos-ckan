f2cfc930fabef6d5eaea2748a682fe55ff919f78
Ricardo Ireno <ricardoireno@outlook.com>
2022-01-22 14:19:51 -0300

    Update aCkan.ts

--- src/services/apiCkan.ts
+++ src/services/apiCkan.ts
@@ -1,8 +1,3 @@
 import axios from "axios"
 const ApiCkan = axios.create( {baseURL: process.env.NEXT_PUBLIC_BASEURL} )
-
-// const ApiCkan = axios.create({  
-//   baseURL: process.env.NEXT_PUBLIC_BASEURL,
-//   headers: { 'Authorization': 'a8b0ff18-2234-4391-80a4-e61168d4419f' }
-//   })
 export default ApiCkan
