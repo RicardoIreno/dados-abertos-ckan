3fe1735b6be5879899f39225dca07c080bb2be4d
--global <--global>
2022-02-16 13:05:29 -0300

    Fixing duplicate axios instance

--- src/pages/api/datasets/search/[s].ts
+++ src/pages/api/datasets/search/[s].ts
@@ -1,26 +1,8 @@
 import  {NextApiRequest, NextApiResponse} from 'next'
-import axios from 'axios'  
-import { SearchDatasetResponse } from 'types'
-
-const URL = 'http://localhost:5000/api/3/action/'
-const Call = axios.create( { baseURL: URL } )
-
-
-async function callApi( s: string | string[] ) {
-  return Call.get<SearchDatasetResponse>(`package_search?q=${s}`)
-  .then( res => res.data.result );
-}
+import {searchDataset} from 'services/adaptersCkan'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {q, s} = req.query
-  const resp = await callApi(s)
+  const resp = await searchDataset(s)
   return res.json( resp )
-
-  // try {
-  //   const resp = await searchDataset(s.toString())
-  //   return res.status(200).json(resp)
-
-  // } catch (err) {
-  //   return res.json({messgae: 'Deu ruim', error: err})
-  // }
 }
