d57ae578d6b74bcb2f5c2e340231c3a24ba032f0
--global <--global>
2022-02-18 09:38:44 -0300

    Set to call Dados Abertos

--- src/services/ApiCkan.ts
+++ src/services/ApiCkan.ts
@@ -1,3 +1,4 @@
 import axios from "axios"
-const ApiCkan = axios.create( {baseURL: `http://localhost:5000/api/3/action/`} )
+// const ApiCkan = axios.create( {baseURL: `http://localhost:5000/api/3/action/`} )
+const ApiCkan = axios.create( {baseURL: `http://dadosabertos.unifesp.br/api/3/action/`} )
 export default ApiCkan
