59bb9f469bf6c0c3384bbd076e918d0e50fd8964
Ricardo Ireno <ricardoireno@outlook.com>
2022-10-07 13:58:10 -0300

    Removing non used components

--- src/components/SearchOrchestrator.tsx
+++ src/components/SearchOrchestrator.tsx
@@ -1,26 +1,22 @@
-import { useQuery, useQueryClient, useMutation } from 'react-query'
-import { ReactNode, useEffect, useState } from 'react'
-import { searchDataset, searchDatasets } from 'services/ApiMy'
+import { useQuery, useQueryClient } from "react-query"
+import { ReactNode, useEffect } from "react"
+import { searchDatasets } from "services/ApiMy"
 
 type Props = {
-  children: ReactNode
+	children: ReactNode
 }
 
 export default function SearchOrchestrator(p: Props) {
-  const queryClient = useQueryClient()
-  const queryTerm = useQuery<string>('term', {retry: false})
-  const queryTags = useQuery<string[]>('tags', {retry: false})
+	const queryClient = useQueryClient()
+	const queryTerm = useQuery<string>("term", { retry: false })
+	const queryTags = useQuery<string[]>("tags", { retry: false })
 
-  useEffect( () => {
-    if (queryTerm.data || queryTags.data  )
-      searchDatasets(queryTerm.data, queryTags.data)
-        .then( d => queryClient.setQueryData('datasets', d.result )
-      )
-  },[queryTerm.data, queryTags.data ] )
+	useEffect(() => {
+		if (queryTerm.data || queryTags.data)
+			searchDatasets(queryTerm.data, queryTags.data).then((d) =>
+				queryClient.setQueryData("datasets", d.result)
+			)
+	}, [queryTerm.data, queryTags.data])
 
-  return (
-    <>
-     {p.children}
-    </>
-  )
+	return <>{p.children}</>
 }
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react"
+import { ReactNode } from "react"
 import { css } from "../styles/stitches.config"
 
 type Props = {
